97e28372fb4d6f460a5dfff2a42a905c4971ee57
Adelaiten <karoltrzaska19@gmail.com>
2019-03-19 16:08:35 +0100

    get mapping all authors retriever

--- src/main/java/com/codecool/BookShop/controller/AuthorController.java
+++ src/main/java/com/codecool/BookShop/controller/AuthorController.java
@@ -1,4 +1,26 @@
 package com.codecool.BookShop.controller;
 
+import com.codecool.BookShop.model.Author;
+import com.codecool.BookShop.repository.AuthorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+
+@RestController
 public class AuthorController {
+
+    @Autowired
+    private AuthorRepository authorRepository;
+
+    @GetMapping("/authors")
+    public List<Author> retrieveAllAuthors() {
+        return authorRepository.findAll();
+    }
+
+
+
 }
