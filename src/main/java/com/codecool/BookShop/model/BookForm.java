93284b9be3d62df7053e7846fa483b3926cc3e1e
Adelaiten <karoltrzaska19@gmail.com>
2019-03-19 10:20:47 +0100

    Bookform model done

--- src/main/java/com/codecool/BookShop/model/BookForm.java
+++ src/main/java/com/codecool/BookShop/model/BookForm.java
@@ -1,4 +1,29 @@
 package com.codecool.BookShop.model;
 
 public class BookForm {
+    private int id;
+    private String form;
+
+    protected BookForm() {}
+
+    public BookForm(int id, String form) {
+        this.id = id;
+        this.form = form;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getForm() {
+        return form;
+    }
+
+    public void setForm(String form) {
+        this.form = form;
+    }
 }
