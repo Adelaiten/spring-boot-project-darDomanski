ebab4a2f6a744a1c5b4cb7095ef8b32aa3bc69e0
Adelaiten <karoltrzaska19@gmail.com>
2019-03-19 10:18:38 +0100

    genre model done

--- src/main/java/com/codecool/BookShop/model/Genre.java
+++ src/main/java/com/codecool/BookShop/model/Genre.java
@@ -1,4 +1,30 @@
 package com.codecool.BookShop.model;
 
 public class Genre {
+    private int id;
+    private String genre;
+
+    protected Genre(){}
+
+
+    public Genre(int id, String genre) {
+        this.id = id;
+        this.genre = genre;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
 }
