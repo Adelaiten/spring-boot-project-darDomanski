3b592c9d3e8d0050978df50182018338cc6a825a
Adelaiten <karoltrzaska19@gmail.com>
2019-03-19 17:12:43 +0100

    retrieve author by id method

--- src/main/java/com/codecool/BookShop/controller/AuthorController.java
+++ src/main/java/com/codecool/BookShop/controller/AuthorController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 
 @RestController
@@ -17,6 +18,11 @@ public class AuthorController {
     @GetMapping("/authors")
     public List<Author> retrieveAllAuthors() {
         return authorRepository.findAll();
+    }
+
+    @GetMapping("/author/{id}")
+    public Optional<Author> retrieveAuthorById(@PathVariable Long id) {
+        return authorRepository.findById(id);
     }
 
     @PostMapping("/author")
