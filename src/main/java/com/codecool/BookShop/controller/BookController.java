6ffb73db72a54e6ccf38925c148adeed2019585e
Adelaiten <karoltrzaska19@gmail.com>
2019-03-20 10:26:39 +0100

    bookController method post

--- src/main/java/com/codecool/BookShop/controller/BookController.java
+++ src/main/java/com/codecool/BookShop/controller/BookController.java
@@ -3,10 +3,10 @@ package com.codecool.BookShop.controller;
 import com.codecool.BookShop.model.Book;
 import com.codecool.BookShop.repository.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class BookController {
@@ -17,5 +17,16 @@ public class BookController {
     public List<Book> retrieveAllBooks() {
         return bookRepository.findAll();
     }
+
+    @GetMapping("/books/{id}")
+    public Optional<Book> retrieveBookById(@PathVariable Long id) {
+        return bookRepository.findById(id);
+    }
+
+    @PostMapping("/books")
+    public Book addBook(@RequestBody Book book) {
+        return bookRepository.save(book);
+    }
+
 
 }
