057bc149fcf2914f580ca7f44e1e14326848b9fb
Adelaiten <karoltrzaska19@gmail.com>
2019-03-20 13:45:33 +0100

    add book with checking if the genre exists

--- src/main/java/com/codecool/BookShop/controller/BookController.java
+++ src/main/java/com/codecool/BookShop/controller/BookController.java
@@ -1,10 +1,19 @@
 package com.codecool.BookShop.controller;
 
+import com.codecool.BookShop.model.Author;
 import com.codecool.BookShop.model.Book;
+import com.codecool.BookShop.model.Genre;
 import com.codecool.BookShop.repository.BookRepository;
+import com.codecool.BookShop.repository.GenreRepository;
+import com.codecool.BookShop.service.AuthorService;
+import com.codecool.BookShop.service.GenreService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.constraints.Null;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -12,6 +21,10 @@ import java.util.Optional;
 public class BookController {
     @Autowired
     private BookRepository bookRepository;
+
+    @Autowired
+    GenreService genreService;
+
 
     @GetMapping("/books")
     public List<Book> retrieveAllBooks() {
@@ -25,7 +38,29 @@ public class BookController {
 
     @PostMapping("/books")
     public Book addBook(@RequestBody Book book) {
+        String genreName = book.getGenre().getGenre().toLowerCase();
+        book.getGenre().setGenre(genreName);
+        Genre genre = null;
+        List<Genre> genres = new ArrayList<>();
+        genres = getGenreByGenreName(genreName, genres);
+
+        if(genres.size() > 0) {
+            genre = genres.get(0);
+        }
+
+        if(genre !=null) {
+            book.setGenre(genre);
+        }
         return bookRepository.save(book);
+    }
+
+    private List<Genre> getGenreByGenreName(String genreName, List<Genre> genres) {
+        try{
+           genres = genreService.getGenreByGenreName(genreName);
+        }catch (NullPointerException e){
+            System.out.println("Genre does not exist in database");
+        }
+        return genres;
     }
 
 
