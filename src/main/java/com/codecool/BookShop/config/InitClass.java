17d68de5906d8b5cf4fe584d61f9d463a6d828e1
darDomanski <dardomanski@gmail.com>
2019-03-21 10:34:10 +0100

    Implement InitClass

---
+++ src/main/java/com/codecool/BookShop/config/InitClass.java
@@ -0,0 +1,32 @@
+package com.codecool.BookShop.config;
+
+import com.codecool.BookShop.model.Role;
+import com.codecool.BookShop.model.User;
+import com.codecool.BookShop.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class InitClass {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping("init")
+    public String init() {
+        Role roleAdmin = new Role("admin");
+        Role roleUser = new Role("user");
+
+        User user1 = new User("daro", "daro123");
+        user1.addRole(roleAdmin);
+        User user2 = new User("roman", "roman123");
+        user2.addRole(roleUser);
+
+        userRepository.save(user1);
+        userRepository.save(user2);
+
+
+        return "Basic resources initialized!";
+    }
+}
