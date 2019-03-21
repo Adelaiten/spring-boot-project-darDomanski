b48a0c4701b9547e6d6bd71b6e9734ece7d80e62
Adelaiten <karoltrzaska19@gmail.com>
2019-03-21 11:21:25 +0100

    dao get authors by name and surname

--- src/main/java/com/codecool/BookShop/repository/AuthorRepository.java
+++ src/main/java/com/codecool/BookShop/repository/AuthorRepository.java
@@ -2,6 +2,13 @@ package com.codecool.BookShop.repository;
 
 import com.codecool.BookShop.model.Author;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface AuthorRepository extends JpaRepository<Author, Long> {
+    @Query(value = "SELECT * FROM authors WHERE name = :name AND surname = :surname", nativeQuery = true)
+    List<Author> getAuthorsByNameAndSurname(@Param("name") String name, @Param("surname") String surname);
 }
+
