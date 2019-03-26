858da5654bf91baf10c62a39169c333de9ddddd5
Adelaiten <karoltrzaska19@gmail.com>
2019-03-26 10:58:08 +0100

    deleted unused imports

--- src/main/java/com/codecool/BookShop/BookShopApplication.java
+++ src/main/java/com/codecool/BookShop/BookShopApplication.java
@@ -2,10 +2,7 @@ package com.codecool.BookShop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import java.io.File;
 
 @SpringBootApplication
 public class BookShopApplication {
